7a89c4c7760d7ad31d730f74df9b9e883333f40a
Weniasss <233299@edu.p.lodz.pl>
2022-12-19 22:09:06 +0100

    commit_done

--- lib/map.dart
+++ lib/map.dart
@@ -10,6 +10,8 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoder/geocoder.dart' as geoCo;
 
+import 'package:intl/intl.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -37,7 +39,9 @@ class _HomepageState1 extends State<Homepage1> {
     _selectedVal = _productSizedList[0];
   }
   Completer<GoogleMapController> _controller = Completer();
+
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
   // late Position position;
   //late String address;
   late String country;
@@ -66,66 +70,178 @@ class _HomepageState1 extends State<Homepage1> {
   CollectionReference _referenceList =
       FirebaseFirestore.instance.collection('location');
 
+  String tdata =
+      DateFormat("HH:mm:ss").format(DateTime.now().add(Duration(minutes: 30)));
+
   Future getDataBase() async {
     await FirebaseFirestore.instance
         .collection("location")
         .get()
         .then((snapshot) => snapshot.docs.forEach((element) {
+              final Timestamp timestamp = element.data()["Time"] as Timestamp;
+              final DateTime dateTime = timestamp.toDate();
+
               MarkerId markerId = MarkerId(
                   element.data()["latitude"].toString() +
                       element.data()["longitude"].toString());
               Marker _marker = Marker(
-                  markerId: markerId,
-                          onTap: () {
+                markerId: markerId,
+                onTap: () {
+                  showModalBottomSheet(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20))),
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (context) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              width: 20,
+                              height: 20,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  left: 20,
+                                  bottom: 5,
+                                  right: 20,
+                                  top: 5), //apply padding to all four sides
+                              child: Text(
+                                " Type : " + element.data()["Type"].toString(),
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  left: 20,
+                                  bottom: 10,
+                                  right: 20,
+                                  top: 5), //apply padding to all four sides
+                              child: Text(
+                                  " Description : " +
+                                      element.data()["Description"].toString(),
+                                  style: TextStyle(fontSize: 18)),
+                            ),
+                            ListTile(
+                              title: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 20,
+                                          bottom: 5,
+                                          right: 20,
+                                          top:
+                                              5), //apply padding to all four sides
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.green[600],
+                                            textStyle: const TextStyle(
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10))),
+                                        child: const Text('Up'),
+                                        onPressed: () async {
+                                          print(element.reference.id +
+                                              "_________________________");
 
-          _customInfoWindowController.addInfoWindow!(
-            Container(
-              height: 300,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 300,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(10.0)),
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            LatLng(
-                      element.data()["latitude"], element.data()["longitude"]),
-          );
-        },
-                  position: LatLng(
-                      element.data()["latitude"], element.data()["longitude"]),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(
-                      BitmapDescriptor.hueCyan),
-                  infoWindow: InfoWindow(title: "Marker"));
-              setState(() {
-                markers[markerId] = _marker;
-              });
+                                          FirebaseFirestore.instance
+                                              .collection('location')
+                                              .doc(element.reference.id)
+                                              .update({
+                                            'Time': DateTime.now()
+                                                .add(Duration(minutes: 30)),
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          left: 20,
+                                          bottom: 5,
+                                          right: 20,
+                                          top:
+                                              5), //apply padding to all four sides
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.red[600],
+                                            textStyle: const TextStyle(
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10))),
+                                        child: const Text('Down'),
+                                        onPressed: () async {
+                                          MarkerId val = MarkerId(element
+                                                  .data()["latitude"]
+                                                  .toString() +
+                                              element
+                                                  .data()["longitude"]
+                                                  .toString());
+
+                                          print(markers[val]);
+                                          print("__________________________");
+
+                                          FirebaseFirestore.instance
+                                              .collection('location')
+                                              .doc(element.reference.id)
+                                              .delete();
+
+                                          markers.removeWhere(
+                                              (key, value) => key == val);
+                                          setState(() {
+
+                                          });
+
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8),
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context)
+                                        .viewInsets
+                                        .bottom),
+                              ),
+                            )
+                          ],
+                        );
+                      });
+                },
+                position: LatLng(
+                    element.data()["latitude"], element.data()["longitude"]),
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueCyan),
+              );
+              if (dateTime.isAfter(DateTime.now())) {
+                setState(() {
+                  markers[markerId] = _marker;
+                });
+              }
 
               print(element.data()["Country"]);
             }));
   }
 
-  void getMarkers(double lat, double long) {
+  void getMarkers(double lat, double long, String description, String type) {
     MarkerId markerId = MarkerId(lat.toString() + long.toString());
     Marker _marker = Marker(
         markerId: markerId,
         position: LatLng(lat, long),
-
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
         infoWindow: InfoWindow(snippet: "addres"));
     setState(() {
@@ -180,7 +296,7 @@ class _HomepageState1 extends State<Homepage1> {
                 child: Stack(children: <Widget>[
                   GoogleMap(
                     onTap: (tapped) async {
-                      
+                      getDataBase();
                       latitude = tapped.latitude;
                       longtude = tapped.longitude;
 
@@ -189,15 +305,12 @@ class _HomepageState1 extends State<Homepage1> {
                       var adrress = await geoCo.Geocoder.local
                           .findAddressesFromCoordinates(coordinated);
                       var firstAddress = adrress.first;
-                      getMarkers(tapped.latitude, tapped.longitude);
+                      getMarkers(
+                          tapped.latitude,
+                          tapped.longitude,
+                          emailController.text.trim(),
+                          passwordController.text.trim());
                       _customInfoWindowController.hideInfoWindow!();
-                      // await FirebaseFirestore.instance.collection('location').add({
-                      //   'latitude': tapped.latitude,
-                      //   'longitude': tapped.longitude,
-                      //   'Address': firstAddress.addressLine,
-                      //   'Country': firstAddress.countryName,
-                      //   'PostalCode': firstAddress.postalCode,
-                      // });
                     },
                     onCameraMove: (position) {
                       _customInfoWindowController.onCameraMove!();
@@ -326,7 +439,13 @@ class _HomepageState1 extends State<Homepage1> {
                                                 .findAddressesFromCoordinates(
                                                     coordinated);
                                             var firstAddress = adrress.first;
-                                            getMarkers(latitude, longtude);
+
+                                            getMarkers(
+                                                latitude,
+                                                longtude,
+                                                emailController.text.trim(),
+                                                passwordController.text.trim());
+
                                             await FirebaseFirestore.instance
                                                 .collection('location')
                                                 .add({
@@ -342,6 +461,8 @@ class _HomepageState1 extends State<Homepage1> {
                                                   firstAddress.countryName,
                                               'PostalCode':
                                                   firstAddress.postalCode,
+                                              'Time': DateTime.now()
+                                                  .add(Duration(minutes: 30)),
                                             });
 
                                             // Navigator.pop(context);
